fc31b76aa6a9013f5292cf0b8aef902e8aa48a84
SebasUQ <juans.gonzalezm@uqvirtual.edu.co>
2024-09-25 18:15:28 -0500

    Ventana Tripulacion

--- src/main/java/co/edu/uniquindio/caribe_airlines/viewController/PanelTripulacionController.java
+++ src/main/java/co/edu/uniquindio/caribe_airlines/viewController/PanelTripulacionController.java
@@ -1,4 +1,19 @@
 package co.edu.uniquindio.caribe_airlines.viewController;
 
+import javafx.scene.control.*;
+
 public class PanelTripulacionController {
+    public TextField txtNom;
+    public TextField txtID;
+    public TextField txtDirec;
+    public TextField txtCorreo;
+    public DatePicker fNacimiento;
+    public TextField txtEstud;
+    public TreeTableView ListaTripulantes;
+    public TreeTableColumn Nombres;
+    public TreeTableColumn IDs;
+    public TreeTableColumn Correos;
+    public Button btnAgregar;
+    public Button btnEliminar;
+    public Button btnActualizar;
 }
