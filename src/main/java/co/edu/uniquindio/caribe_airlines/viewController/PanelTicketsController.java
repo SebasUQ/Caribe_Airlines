380f2a20609cde51bd28b5161e7e68f49a66b5ac
SebasUQ <juans.gonzalezm@uqvirtual.edu.co>
2024-09-25 17:45:04 -0500

    Ventana Flotas

--- src/main/java/co/edu/uniquindio/caribe_airlines/viewController/PanelTicketsController.java
+++ src/main/java/co/edu/uniquindio/caribe_airlines/viewController/PanelTicketsController.java
@@ -1,4 +1,7 @@
 package co.edu.uniquindio.caribe_airlines.viewController;
 
+import javafx.scene.layout.AnchorPane;
+
 public class PanelTicketsController {
+    public AnchorPane PanelCambiante;
 }
