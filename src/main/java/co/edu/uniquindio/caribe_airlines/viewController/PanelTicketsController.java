dcfcf7a61a98d6e92fcfb1b684df8df58e018834
SebasUQ <juans.gonzalezm@uqvirtual.edu.co>
2024-09-28 12:08:15 -0500

    panel dinamico (actualizado)
    ya hay contenido en el primer panel para los tickets

--- src/main/java/co/edu/uniquindio/caribe_airlines/viewController/PanelTicketsController.java
+++ src/main/java/co/edu/uniquindio/caribe_airlines/viewController/PanelTicketsController.java
@@ -3,14 +3,31 @@ package co.edu.uniquindio.caribe_airlines.viewController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
 
 public class PanelTicketsController {
+
+    // Componenetes
     public Button btnSiguiente;
     public AnchorPane panelTickets;
+    public ComboBox combModalidad;
+    public ComboBox combServicio;
+    public ComboBox combOrigen;
+    public ComboBox combDestino;
+    public DatePicker fInicio;
+    public DatePicker fRetorno;
+    public TextField numPersonas;
+    public TableView tablaVuelos;
+    public TableColumn columOrigen;
+    public TableColumn columDestino;
+    public TableColumn columDuracion;
+    public TableColumn columSalida;
+
+    //----------------------------------------------------------------------------------------------//
+
 
     public void cambiarPanel (AnchorPane panel){
         this.panelTickets = panel;
